13b5bbb4674014d088892e324cb63b38a422bab7
likili <byglaev@gmail.com>
2019-01-13 00:32:26 +0300

    SWITCH-7 создать models для разделов

--- blocks/views.py
+++ blocks/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
-
+from django.http import HttpResponse
+from django.contrib import messages
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 
